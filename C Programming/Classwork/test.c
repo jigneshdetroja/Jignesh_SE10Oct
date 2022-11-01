e9d60ea4b7042b9666b93b48f20c7ee28ec2d22b
jigneshdetroja <jigneshthakor435@gmail.com>
2022-11-01 21:29:12 +0530

    Create test.c

---
+++ C Programming/Classwork/test.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void main()
+{
+	char name [10]="tops";
+	int age       = 24;
+	float salary  = 24577.75;
+	
+	printf("Name  : %s\n" ,name);
+	printf("Age   : %d\n" ,age);
+	printf("Salary: %.2f\n" ,salary);
+}

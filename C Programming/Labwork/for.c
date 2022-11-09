0712d8d58fd37173d517393a93e78f810944f260
jigneshdetroja <jigneshthakor435@gmail.com>
2022-11-09 21:24:48 +0530

    Create for.c

---
+++ C Programming/Labwork/for.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/*
+
+for loop:
+
+	syntax:
+for(intialization;condition;updation)
+         {
+         statement;
+     	 }
+     	 
+     	 
+*/
+void main()
+
+{
+	int i;
+	
+	for(i=100;i>=1;i--)
+	{
+		printf("%d\n" ,i);
+	}
+}

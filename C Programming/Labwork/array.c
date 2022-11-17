70595b2a8e5620e9dc4e5739fc6e40d072619f9e
jigneshdetroja <jigneshthakor435@gmail.com>
2022-11-17 21:53:53 +0530

    Create array.c

---
+++ C Programming/Labwork/array.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/*
+
+Array : int marss[5];
+
+-> Array is a name at memory location which stores multiple value of same type.
+-> Array index starts with 0.
+
+*/
+
+void main()
+
+{
+	int mars[5] = {54,95,45,65,56};
+	printf("%d\t",mars[0]);
+	printf("%d\t",mars[1]);
+	printf("%d\t",mars[2]);
+	printf("%d\t",mars[3]);
+	printf("%d\t",mars[4]);
+}

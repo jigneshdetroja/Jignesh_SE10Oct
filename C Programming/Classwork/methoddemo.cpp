bddb0dcf7a84af68096ba393b86b9341237c05fd
jigneshdetroja <115604810+jigneshdetroja@users.noreply.github.com>
2022-12-09 09:03:27 +0530

    Create methoddemo.cpp

---
+++ C Programming/Classwork/methoddemo.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+class Sample{
+	public:
+		
+		void show()
+		{
+			cout<<"This is Show";
+		}
+}; 
+
+int main()
+{
+	Sample sam;
+	sam.show();
+	return 0;
+}

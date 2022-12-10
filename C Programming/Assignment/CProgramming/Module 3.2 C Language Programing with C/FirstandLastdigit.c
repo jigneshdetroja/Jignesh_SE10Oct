cb95f051ffb3ac826ade40ce38e735c85b3e21c6
jigneshdetroja <115604810+jigneshdetroja@users.noreply.github.com>
2022-12-10 20:08:22 +0530

    Create FirstandLastdigit.c

---
+++ C Programming/Assignment/CProgramming/Module 3.2 C Language Programing with C/FirstandLastdigit.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int n, sum=0, firstdigit, lastdigit;
+	printf("Enter Number = ");
+	scanf("%d",&n);
+	lastdigit = n % 10;
+	while(n >=10)
+	{
+		n = n / 10;
+	}
+	firstdigit = n;
+	printf("first digit = %d\n", firstdigit);
+	printf("last digit = %d\n", lastdigit);
+	
+}

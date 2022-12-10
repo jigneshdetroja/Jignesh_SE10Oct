24b76e0628f1c9980ee06c539cdd94a77c285529
jigneshdetroja <115604810+jigneshdetroja@users.noreply.github.com>
2022-12-10 20:09:37 +0530

    Create NumbersinReverseOrder.c

---
+++ C Programming/Assignment/CProgramming/Module 3.2 C Language Programing with C/NumbersinReverseOrder.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+	int Number, i;
+	
+	printf("\n Please Enter the Maximum Integer Value (Upper Limit) : ");
+	scanf("%d", &Number);
+	
+	printf("\n List of Numbers from %d to 1 are \n", Number);
+	for(i = Number; i >= 1; i--)
+	{
+		printf("%d\t", i);
+	}
+}

11b8f3330cb6ba55426bd1fc7dafb2e25ad4215e
jigneshdetroja <115604810+jigneshdetroja@users.noreply.github.com>
2022-12-10 20:08:56 +0530

    Create MaxNumber.c

---
+++ C Programming/Assignment/CProgramming/Module 3.2 C Language Programing with C/MaxNumber.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("Enter Two Numbers: ");
+	scanf("%d%d",&a,&b);
+	if(a>b)
+	{
+	else
+	{
+		printf("Maximum Number : %d",b);
+	}
+	return 0;
+
+}

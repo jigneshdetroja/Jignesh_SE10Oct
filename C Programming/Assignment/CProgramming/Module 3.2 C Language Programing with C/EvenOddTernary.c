f32755e5611233fd731fa4dcbffdfa65017dc8ca
jigneshdetroja <115604810+jigneshdetroja@users.noreply.github.com>
2022-12-11 22:21:51 +0530

    Update EvenOddTernary.c

--- C Programming/Assignment/CProgramming/Module 3.2 C Language Programing with C/EvenOddTernary.c
+++ C Programming/Assignment/CProgramming/Module 3.2 C Language Programing with C/EvenOddTernary.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 
-int main()
+void main()
 {
 	int n;
 	
@@ -8,7 +8,7 @@ int main()
 	scanf("%d", &n);
 	
 	(n%2==0)?
-	(printf("%d is Even number\n", n)) : (printf("%d is Odd numbern", n));
+	(printf("%d is Even number\n", n)) : (printf("%d is Odd number\n", n));
 	
-	return 0;
+
 }

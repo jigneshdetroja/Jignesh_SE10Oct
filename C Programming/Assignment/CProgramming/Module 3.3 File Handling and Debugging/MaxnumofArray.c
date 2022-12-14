2efc65c584b4ca8aa6e6228d1f70a4b64eb64b0d
jigneshdetroja <115604810+jigneshdetroja@users.noreply.github.com>
2022-12-14 22:42:49 +0530

    Update MaxnumofArray.c

--- C Programming/Assignment/CProgramming/Module 3.3 File Handling and Debugging/MaxnumofArray.c
+++ C Programming/Assignment/CProgramming/Module 3.3 File Handling and Debugging/MaxnumofArray.c
@@ -2,21 +2,16 @@
 
 void main()
 {
-	int arr[10], i, Largest;
-	printf("\n Enter 10 array Elements\n");
-
-	for(i=0; i<10; i++)
+	int array[] = {10,25,9,56,85};
 	
-		scanf("%d",&arr[i]);
-	i=0(arr);
-	Largest = arr[i];
-	for(i=1; i<10; i++)
+	int length = sizeof(array)/sizeof(array[0]);
+	
+	int max = array[0];
+	
+	for (int i = 0; i < length; i++)
 	{
-		if(Largest<arr[i]);
-		{
-			Largest=arr[i];
-			Position= i;
-	    }
+		if(array[i] > max)
+		max = array[i];
 	}
-	printf("\nLargest element in an Array = %d", Largest);
-	printf("\nIndex position of the Largest element = %d", Position); 
+	printf("Maximum number of given in array is : %d\n",max);
+}

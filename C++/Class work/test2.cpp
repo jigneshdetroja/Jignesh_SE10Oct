8a04ae20cc673ef5a53fd9ba4cb2b7537970a856
jigneshdetroja <115604810+jigneshdetroja@users.noreply.github.com>
2023-01-17 21:13:02 +0530

    Create test2.cpp

---
+++ C++/Class work/test2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+	int a=4;
+	int b=7;
+	
+	cout<<"A = "<<a<<",B = "<<b;
+	cout<<"\nAddition : "<<(a+b);
+	cout<<"\nSubtraction : "<<(a-b);
+	cout<<"\nMultiplication : "<<(a*b);
+	printf("\nDivision : %.2f",((float)a/b));
+    cout<<"\nDivision : "<<((float)a/b);
+	
+    retutn 0;
+}

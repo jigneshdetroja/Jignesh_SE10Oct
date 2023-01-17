129346bcdf3bbcbc4c47a71884be1f025035e71c
jigneshdetroja <115604810+jigneshdetroja@users.noreply.github.com>
2023-01-17 21:11:37 +0530

    Create getline.cpp

---
+++ C++/Lab work/getline.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+class Details{
+	public:
+		string name;
+		void getName()
+		{
+			cout<<"\nEnter Your Name.";
+			getline(cin,name);
+		}
+		
+		void showName()
+		{
+			cout<<"\nName is : "<<name;
+		}
+};
+int main()
+{
+	Details.getName()
+	obj
+	
+	return 0;
+}

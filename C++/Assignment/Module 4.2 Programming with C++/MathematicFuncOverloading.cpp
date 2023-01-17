c77be61aa911587ae95b7ea2d369b93499d9a5c9
jigneshdetroja <115604810+jigneshdetroja@users.noreply.github.com>
2023-01-17 22:31:29 +0530

    Create MathematicFuncOverloading.cpp

---
+++ C++/Assignment/Module 4.2 Programming with C++/MathematicFuncOverloading.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+	double num1, num2;
+	cout<< "Enter two numbers :: ";
+	cin>> num1>>num2;
+	
+	cout<< num1 <<"+"<< num2 << " = "<< num1+num2 <<endl;
+	
+	cout<< num1 <<"-"<< num2 << " = "<< num1-num2 <<endl;
+	
+	cout<< num1 <<"*"<< num2 << " = "<< num1*num2 <<endl;
+	
+	cout<< num1 <<"/"<< num2 << " = "<< num1/num2 <<endl;
+	
+	return 0;
+}

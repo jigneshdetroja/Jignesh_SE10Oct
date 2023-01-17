df889021a24300d4f6510f28b46cdaf7d13e3022
jigneshdetroja <115604810+jigneshdetroja@users.noreply.github.com>
2023-01-17 22:29:02 +0530

    Create polynote.cpp

---
+++ C++/Class work/polynote.cpp
@@ -0,0 +1,22 @@
+/*
+   Polymorphism : (Many + Forms)
+   
+   				Poly : Many
+   				morphism : Forms
+   				
+   	e.g a bird and aeroplane.
+   	
+   	2 Types :
+   	
+   	1) Compile Time Polymorphism
+   			- It is also called as Static Polymorphism or Early Binding
+   			
+   		e.g 1) Method Overloading.
+   			2) Operator Overloading.
+   			
+   	2) Runtime Polymorphism
+   			- It is also called as Dynamic Polymorphism or Late Binding.
+   			
+   		e.g 1) Method Overriding.
+
+*/

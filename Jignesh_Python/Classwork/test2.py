d0c92cd4c793f97635d3f266a06a20b1e264a0e1
jigneshdetroja <115604810+jigneshdetroja@users.noreply.github.com>
2023-02-26 22:08:55 +0530

    Create test2.py

---
+++ Jignesh_Python/Classwork/test2.py
@@ -0,0 +1,34 @@
+"""
+n1 = 52
+n2 = 25
+print ("*"*60)
+print("Addition: ",(n1+n2))
+print("Subtraction: ",(n1-n2))
+print("Divition: ",(n1/n2))
+print("Multiplication: ",(n1*n2))
+print ("*"*60)
+print("Type : ",type(n1))
+"""
+"""
+n1 = int(input("Enter Value of A : "))
+n2 = int(input("Enter Value of B : "))
+print ("*"*60)
+print("Addition: ",(n1+n2))
+print("Subtraction: ",(n1-n2))
+print("Divition: ",(n1/n2))
+print("Multiplication: ",(n1*n2))
+print ("*"*60)
+print("Type : ",type(n1))
+
+"""
+a = int(input("Enter A : "))
+b = int(input("Enter B : "))
+print("Before Swapping A = ",a,", B = ",b)
+
+"""
+t = a
+a = b
+b = t
+"""
+a,b = b,a
+print("After Swapping A = ",a,", B = ",b)

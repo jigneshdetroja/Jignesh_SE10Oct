35029070c476275de158bb4d4b11a9ce58b422e3
jigneshdetroja <115604810+jigneshdetroja@users.noreply.github.com>
2023-02-26 22:08:46 +0530

    Create stingdemo.py

---
+++ Jignesh_Python/Classwork/stingdemo.py
@@ -0,0 +1,17 @@
+s = "Tops Technologies"
+print (s)
+
+print(s.capitalize())
+print(s.casefold())
+print(s.center(50,"*"))
+print(s.startswith("t"))
+print(s.endswith("es"))
+print(s.find("log"))
+print("tops123".isalpha())
+print("tops".isalpha())
+print("tops123".isalnum())
+print("123".isnumeric())
+print("tops".islower())
+print("tops".isupper())
+print("Tops Techno".istitle())
+print("hello".replace("l","w"))

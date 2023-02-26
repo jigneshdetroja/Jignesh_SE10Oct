52a8d5ea9419f9dda556a7544bd26d168c6532f3
jigneshdetroja <115604810+jigneshdetroja@users.noreply.github.com>
2023-02-26 22:07:30 +0530

    Create andor.py

---
+++ Jignesh_Python/Classwork/andor.py
@@ -0,0 +1,14 @@
+"""
+#continue:
+for i in range(1,10):
+    if i%2==0:
+        continue
+    else:
+        print(i)
+"""
+
+for i in range(1,10):
+    if i==4 or i==7:
+        continue
+    else:
+        print("I : ",i)

475eff7bf18d0c33a0ab68a674a5529a3465c0aa
jigneshdetroja <115604810+jigneshdetroja@users.noreply.github.com>
2023-02-26 22:08:30 +0530

    Create pattern.py

---
+++ Jignesh_Python/Classwork/pattern.py
@@ -0,0 +1,26 @@
+"""
+#pat1
+for i in range(1,10):
+    print("*"*(9-i))
+
+
+#pat2
+for i in range(1,10):
+    print("*"*i)
+
+
+#pat3
+for i in range(1,11):
+    print(" "*(10-i),"*"*i)
+
+
+#pat4
+for i in range(1,11):
+    print(" "*(10-i)," *"*i)
+"""
+
+#pat5
+for i in range(1,10):
+    print(" "*(10-i)," *"*i)
+for i in range(10,0,-1):
+    print(" "*(10-i)," *"*i)

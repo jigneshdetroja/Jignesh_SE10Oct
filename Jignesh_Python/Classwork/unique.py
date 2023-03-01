9d86e8e54873d34e29e6722e37379514775017d2
jigneshdetroja <115604810+jigneshdetroja@users.noreply.github.com>
2023-03-01 22:00:45 +0530

    Create unique.py

---
+++ Jignesh_Python/Classwork/unique.py
@@ -0,0 +1,30 @@
+"""
+l1 = [23,7,23,2,7]
+
+l2 = []
+for i in l1:
+    if i not in l2:
+       l2.append(i)
+print(l2)
+
+l1 = [12,56,48,52,64,3,2]
+
+print(l1[2])
+print(l1[2:6])
+print(l1[-1])
+
+print(l1[-3:-1])
+print(l1[-3:])
+print(l1[-2:])
+
+#nestedlist
+
+l1 = [10,20,[101,102,103,[201,202,203]]]
+print(l1[1])
+print(l1[2][0])
+print(l1[2][3][0])
+"""
+#set
+
+s = {12,23,45,2,12,5,6,23}
+print(s)

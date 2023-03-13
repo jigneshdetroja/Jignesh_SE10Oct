da939baa6f7e2882169b6b1e5e4d80230a92b742
jigneshdetroja <115604810+jigneshdetroja@users.noreply.github.com>
2023-03-13 23:04:49 +0530

    Create AreaofRectangleCircleandSquare.c

---
+++ C Programming/Assignment/Module 3.1 C Language Fundamental/AreaofRectangleCircleandSquare.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void main()
+{
+	float length, width, area;
+	
+	printf("Enter length of rectangle\n");
+	scanf("%2f",&length);
+	
+	printf("Enter width of rectangle\n");
+	scanf("%2f",&width);
+	
+	area = length*width;
+	
+	printf("Area of Rectangle is %2f\n",area);
+	
+}

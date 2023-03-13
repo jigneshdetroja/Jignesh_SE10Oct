46631cce64425a85472e92487ef1f4c5452f4615
jigneshdetroja <115604810+jigneshdetroja@users.noreply.github.com>
2023-03-13 23:05:23 +0530

    Create calc.exe

---
+++ C Programming/Assignment/Module 3.1 C Language Fundamental/daysintoyears.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void main()
+{
+	int days, years, weeks;
+	days = 1329;
+	
+	
+	years = days / 365;
+	weeks = (days % 365) / 7;
+	days  = days- ((years*365) + (weeks*7));
+	
+	printf("Years : %d\n", years);
+	printf("Weeks : %d\n", weeks);
+	printf("Days  : %d\n", days);
+	
+
+}

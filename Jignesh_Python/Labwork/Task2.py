aeadc79f2ebf4bc50e53f27710182c48fd224ca1
jigneshdetroja <115604810+jigneshdetroja@users.noreply.github.com>
2023-03-13 23:11:37 +0530

    Create Task2.py

---
+++ Jignesh_Python/Labwork/Task2.py
@@ -0,0 +1,15 @@
+#Accept two numbers and check entered number is positive or negative
+s1 = int(input("Enter First A : "))
+s2 = int(input("Enter Second B : "))
+if s1>s2:
+    print("\nIt is +ve")
+else:
+    print("\nIt is -ve")
+
+
+#Accept two numbers and check entered number is even or odd
+n = int(input("Enter Number : "))
+if n%2==0:
+    print(n," is Even")
+else:
+    print(n," is Odd")

ebb51340f6066157329de76e53cd3a7cd5cfcb42
jigneshdetroja <115604810+jigneshdetroja@users.noreply.github.com>
2023-03-13 23:10:56 +0530

    Update test1.py

--- Jignesh_Python/Classwork/test1.py
+++ Jignesh_Python/Classwork/test1.py
@@ -2,6 +2,6 @@ n1 = 52
 n2 = 25
 print ("*"*60)
 print("Addition: ",(n1+n2))
-print("Subtraction: "(n1-n2))
-print("Divition: "(n1/n2))
-print("Multiplication: "(n1*n2))
+print("Subtraction: ",(n1-n2))
+print("Divition: ",(n1/n2))
+print("Multiplication: ",(n1*n2))

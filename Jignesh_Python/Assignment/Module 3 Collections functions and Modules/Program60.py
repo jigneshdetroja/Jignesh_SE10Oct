ff6b7606b87069f3feebf410fe0e511812865077
jigneshdetroja <115604810+jigneshdetroja@users.noreply.github.com>
2023-05-07 18:26:07 +0530

    Create Program60.py

---
+++ Jignesh_Python/Assignment/Module 3 Collections functions and Modules/Program60.py
@@ -0,0 +1,8 @@
+#Python program to calculate the area of a trapezoid 
+height = float(input("Enter height : "))
+b = float(input("Enter base : "))
+b2 = float(input("Enter base 2 : "))
+
+a = ((b+b2)/2)*height
+
+print("Area is : ",a)

17bfd08779cb054f20384db1f347b822f076dc48
jigneshdetroja <115604810+jigneshdetroja@users.noreply.github.com>
2023-05-07 18:25:56 +0530

    Create Program58.py

---
+++ Jignesh_Python/Assignment/Module 3 Collections functions and Modules/Program58.py
@@ -0,0 +1,16 @@
+import random  # import random
+
+file = open("random_file.txt","w")
+file.write("Added Second Line\n")
+file.write("Added Third Line\n")
+file.write("Added Fourth Line\n")
+file.write("Added Fifth Line\n")
+file.write("Added Sixth Line\n")
+file.close()
+
+# Function For Get a Random line from a file
+def read_random_line(line):
+    lines = open(line).read().splitlines()
+    return random.choice(lines)
+
+print(read_random_line('random_file.txt'))
